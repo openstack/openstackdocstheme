49fda78a27391b4c3747b3a2ebbcb656593fdeb8
Doug Hellmann <doug@doughellmann.com>
2015-01-28 16:49:22 -0500

    Don't force-inject the theme
    Let the user configure their project to use the theme, and don't
    automatically assume they want to use our theme just because the package
    is present. This allows this theme to live with oslosphinx, which forces
    the theme setting.

--- openstackdocstheme/__init__.py
+++ openstackdocstheme/__init__.py
@@ -15,25 +15,8 @@
 import os
 
 
-def builder_inited(app):
-    theme_dir = os.path.join(os.path.dirname(__file__), 'theme')
-    app.info('Using openstackdocs theme from %s' % theme_dir)
-    # Insert our theme directory at the front of the search path and
-    # force the theme setting to use the one in the package unless
-    # another openstack theme is already selected. This is done here,
-    # instead of in setup(), because conf.py is read after setup()
-    # runs, so if the conf contains these values the user values
-    # overwrite these. That's not bad for the theme, but it breaks the
-    # search path.
-    app.config.html_theme_path.insert(0, theme_dir)
-    # Set the theme name
-    if not app.config.html_theme.startswith('openstack'):
-        app.config.html_theme = 'openstackdocs'
-    # Re-initialize the builder, if it has the method for setting up
-    # the templates and theme.
-    if hasattr(app.builder, 'init_templates'):
-        app.builder.init_templates()
-
-
-def setup(app):
-    app.connect('builder-inited', builder_inited)
+def get_html_theme_path():
+    """Return the directory containing our HTML theme."""
+    cur_dir = os.path.abspath(os.path.dirname(__file__))
+    d = os.path.join(cur_dir, 'theme')
+    return d
