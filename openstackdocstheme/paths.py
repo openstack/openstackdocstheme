433fa33f6c5b1223aca93c82c52edbbfb3ead216
Stephen Finucane <sfinucan@redhat.com>
2018-02-15 16:18:28 +0000

    Move code out of '__init__'
    We shouldn't be placing stuff here. Let's split it out, in preparation
    for some other functionality.
    
    Imports are retained for older applications.
    
    Change-Id: I79bd88d0dc10d485022fa36fe869b9b73ade3e82

---
+++ openstackdocstheme/paths.py
@@ -0,0 +1,36 @@
+# Copyright 2018 Red Hat, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os.path
+
+
+def get_pkg_path():
+    return os.path.abspath(os.path.dirname(__file__))
+
+
+def get_html_theme_path():
+    """Return the directory containing HTML theme for local builds."""
+    return os.path.join(get_pkg_path(), 'theme')
+
+
+def get_pdf_theme_path():
+    """Return the directory containing PDF theme for local builds."""
+    args = ['theme', 'openstackdocs_pdf', 'pdftheme']
+    return os.path.join(get_pkg_path(), *args)
+
+
+def get_openstack_logo_path():
+    """Return the directory containing openstack logo for local builds."""
+    args = ['theme', 'openstackdocs_pdf', 'openstack-logo-full.png']
+    return os.path.join(get_pkg_path(), *args)
