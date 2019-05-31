9e64a25ec3c6af142b08f02146864e5745944de6
Stephen Finucane <sfinucan@redhat.com>
2019-05-31 18:43:14 +0100

    js: Remove dead or unused JS
    Remove all the commented out stuff, which is clearly not coming back,
    along with code that was applied to elements with the ID of
    'toggle-definitions', none of which exist. This in turns allows us to
    remove all the webuiPopover stuff since that only applied to elements
    with the 'gloss' class, which the 'toggle-definitions' code was
    creating.
    
    Change-Id: Ie0780e41194e5add90c8ea6f63dc496acbb0b85a
    Signed-off-by: Stephen Finucane <sfinucan@redhat.com>

--- openstackdocstheme/theme/openstackdocs/static/js/docs.js
+++ openstackdocstheme/theme/openstackdocs/static/js/docs.js
@@ -2,80 +2,11 @@
 $(".docs-sidebar-section-title").click(function () {
     $('.docs-sidebar-section').not(this).closest('.docs-sidebar-section').removeClass('active');
     $(this).closest('.docs-sidebar-section').toggleClass('active');
-// Bug #1422454
-// Commenting out next line, the default behavior which was preventing links
-// from working.
-//    event.preventDefault();
-});
-
-/* Bug #1422454
-   The toggle functions below enable the expand/collapse, but for now
-   there's no easy way to get deeper links from other guides. So,
-   commenting both toggle functions out.
-// Toggle 1st sub-sections
-$(".docs-sidebar-section ol lh").click(function () {
-    $('.docs-sidebar-section ol').not(this).closest('.docs-sidebar-section ol').removeClass('active');
-    $(this).closest('.docs-sidebar-section ol').toggleClass('active');
-    if ($('.docs-has-sub').hasClass('active')) {
-      $(this).closest('.docs-sidebar-section ol li').addClass('open');
-    }
-    event.preventDefault();
-});
-
-// Toggle 2nd sub-sections
-$(".docs-sidebar-section ol > li > a").click(function () {
-    $('.docs-sidebar-section ol li').not(this).removeClass('active').removeClass('open');
-    $(this).closest('.docs-sidebar-section ol li').toggleClass('active');
-    if ($('.docs-has-sub').hasClass('active')) {
-      $(this).closest('.docs-sidebar-section ol li').addClass('open');
-    }
-    event.preventDefault();
-});
-
-/* Bug #1417291
-   The rule below creates a shaded plus sign next to
-   a numbered sublist of a bulleted list.
-   It's probably there to implement expand/collapse of
-   list items, but unfortunately it affects also those
-   lists where expand/collapse is not intended.
-
-   I am commenting it out to fix this bug. If it causes
-   problems elsewhere, they have to be fixed elsewhere. */
-
-// $('ol > li:has(ul)').addClass('docs-has-sub');
-
-// webui popover
-$(document).ready(function() {
-    function checkWidth() {
-        var windowSize = $(window).width();
-
-        if (windowSize <= 767) {
-            $('.gloss').webuiPopover({placement:'auto',trigger:'click'});
-        }
-        else if (windowSize >= 768) {
-            $('.gloss').webuiPopover({placement:'auto',trigger:'hover'});
-        }
-    }
-
-    // Execute on load
-    checkWidth();
-    // Bind event listener
-    $(window).resize(checkWidth);
 });
 
 // Bootstrap stuff
 $('.docs-actions i').tooltip();
 $('.docs-sidebar-home').tooltip();
-
-// Hide/Toggle definitions
-$("#toggle-definitions").click(function () {
-  $(this).toggleClass('docs-info-off');
-  if ($('.gloss').hasClass('on')) {
-      $('.gloss').removeClass('on').addClass('off').webuiPopover('destroy');
-  } else if ($('.gloss').hasClass('off')) {
-      $('.gloss').removeClass('off').addClass('on').webuiPopover();
-  }
-});
 
 /* BB 150310
    openstackdocstheme provides three types of admonitions, important, note
